c1acbea020c9a2dffb9be27d3efa1c1a9eadeef7
TimAuLait <147064962+TimAuLait@users.noreply.github.com>
2023-10-05 15:25:49 +0200

    Tim

--- form/db.php
+++ form/db.php
@@ -3,6 +3,7 @@
 $user= 'root';
 $password='';
 
+//Connexion à la base de donnée
 try{
     $conn = new PDO('mysql:host=localhost;dbname=gsb_bdd;charset=utf8', $user, $password );
     // echo'Connexion réussi';
